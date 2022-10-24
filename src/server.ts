7547424ca671777ae4c972dc32379a02d766dcd4
Marchi8 <luandeoliveirasoares@gmail.com>
2022-10-24 01:27:56 -0300

    feat: docker config

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import AppDataSource from "./data-source";
 
 (async () => {
 
+    const PORT = process.env.PORT
+
     await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
+        .catch((err) => {
+            console.error("Error during Data Source initialization", err)
+        })
+
+    app.listen(PORT, () => {
+        console.log(`Servidor executando ${PORT}`)
     })
-    
-    app.listen(3000, () => {
-        console.log("Servidor executando")
-    })    
 })()
